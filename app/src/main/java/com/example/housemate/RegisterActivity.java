61481f1f553c033261323a82e832f33552db4824
Antonio <amarincamuna@gmail.com>
2023-05-16 10:53:49 +0200

    Añadido selector de imagen Continuar con la subida de img a firebase

--- app/src/main/java/com/example/housemate/RegisterActivity.java
+++ app/src/main/java/com/example/housemate/RegisterActivity.java
@@ -1,15 +1,25 @@
 package com.example.housemate;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,17 +28,46 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+//import id.zelory.compressor.Compressor;
+import io.grpc.Context;
 
 
 public class RegisterActivity extends AppCompatActivity{
 
     Button btn_register;
+    Button selectorImg;
+
+    Uri uri = null;
+
     EditText name, email, password;
+    ImageView imagenUser;
+
     FirebaseFirestore mFirestore;
     FirebaseAuth mAuth;
+    StorageReference sReference;
+    String storage_path = "userImgs/*";
+
+    Bitmap img_bitmap;
+
+    private static final int COD_SEL_STORAGE = 200;
+    private static final int COD_SEL_IMAGE = 300;
+
+    private Uri img_url;
+    String img = "photo";
+    String idImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +78,14 @@ public class RegisterActivity extends AppCompatActivity{
 
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        sReference = FirebaseStorage.getInstance().getReference();
 
         name = findViewById(R.id.nombre);
         email = findViewById(R.id.correo);
         password = findViewById(R.id.contrasena);
         btn_register = findViewById(R.id.btn_registro);
+        selectorImg = findViewById(R.id.selectorImg);
+        imagenUser = findViewById(R.id.imagenUser);
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +94,7 @@ public class RegisterActivity extends AppCompatActivity{
                 String emailUser = email.getText().toString().trim();
                 String passUser = password.getText().toString().trim();
 
-                if (nameUser.isEmpty() || emailUser.isEmpty() || passUser.isEmpty()){
+                if (nameUser.isEmpty() || emailUser.isEmpty() || passUser.isEmpty() || uri.equals("")){
                     Toast.makeText(RegisterActivity.this, "Complete los datos", Toast.LENGTH_SHORT).show();
                 }else{
 
@@ -64,7 +106,45 @@ public class RegisterActivity extends AppCompatActivity{
                 }
             }
         });
+
+        selectorImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                subirFoto();
+
+            }
+        });
     }
+
+    private void subirFoto(){
+
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType("image/*");
+        mObtenerImg.launch(i);
+
+    }
+
+    private ActivityResultLauncher<Intent> mObtenerImg = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+
+                        Intent data = result.getData();
+                        uri = data.getData();
+                        imagenUser.setImageURI(uri);
+
+                    }
+                    else {
+
+                        Toast.makeText(RegisterActivity.this, "Accion cancelada por el usuario", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+    );
 
     private void registerUser(String nameUser, String emailUser, String passUser) {
 
--- app/src/main/java/com/example/housemate/UserInfoActivity.java
+++ app/src/main/java/com/example/housemate/UserInfoActivity.java
@@ -6,8 +6,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,6 +28,9 @@ public class UserInfoActivity extends AppCompatActivity {
     FirebaseFirestore mFirestore;
 
     TextView emailUser;
+    TextView fechaNacimientoUser;
+
+    Button cerrarSesionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,10 @@ public class UserInfoActivity extends AppCompatActivity {
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
-        emailUser = findViewById(R.id.correoElectronico);
+        emailUser = findViewById(R.id.correoUsuario);
+        fechaNacimientoUser = findViewById(R.id.fechaNacimientoUsuario);
+
+        cerrarSesionBtn = findViewById(R.id.cerrarSesion);
 
         String user = mAuth.getCurrentUser().getUid();
 
@@ -44,6 +53,17 @@ public class UserInfoActivity extends AppCompatActivity {
             public void onEvent(@Nullable DocumentSnapshot document, @Nullable FirebaseFirestoreException error) {
 
                 emailUser.setText(document.getString("email"));
+                fechaNacimientoUser.setText(document.getString("name"));
+
+            }
+        });
+
+        cerrarSesionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+                startActivity(new Intent(UserInfoActivity.this, LoginActivity.class));
 
             }
         });
